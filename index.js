2ab52aed377cba6f36c904a5385251d36b7b5ed8
SvetlanaZt <svetlana.zt23@gmail.com>
2022-07-04 10:48:45 +0300

    mod

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const button = document.querySelector('#button')
-const list = document.querySelector("#card")
+const button = document.getElementById('button')
+const list = document.getElementById('card')
+const code = document.querySelector('.container__code')
 
-list.addEventListener('submit', onClick)
+list.addEventListener('click', onClick)
 
 function onClick() {
     button.classList.remove('is-hidden')
+    code.classList.remove('is-hidden')
 }
